d285d279e9aa479e548df48e60eb7ac7f8a9ab3c
Davi Levy <daviklevy@gmail.com>
2025-10-08 19:24:52 -0300

    (feat) : criando estrutura abse do site

--- App/soma_app/views.py
+++ App/soma_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+# View para o Dashboard (Index)
+def dashboard_index(request):
+    # Aqui você passaria os dados do gráfico quando estiverem prontos.
+    context = {} 
+    return render(request, 'soma_app/dashboard.html', context)
+
+# View para a página "Sobre Nós"
+def sobre_nos(request):
+    context = {}
+    return render(request, 'soma_app/sobre_nos.html', context)
+
+# View para a página "Sobre o SOMA"
+def sobre_soma(request):
+    context = {}
+    return render(request, 'soma_app/sobre_soma.html', context)
